f627c30a23a67258c92a2889be9b31f0d549c7af
herkyl <s.herkyl@gmail.com>
2018-10-20 12:03:00 +0300

    Don't evaluate string

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className={"App-header"}>
+        <div className="App-header">
           <h1>My Chat App</h1>
         </div>
         <Messages

b6b521b7e55fa506523909a2071ab3ae6d7142dc
Serge <s.herkyl@gmail.com>
2023-07-02 14:53:21 +0300

    Improve autofocus usage

--- src/Input.js
+++ src/Input.js
@@ -25,7 +25,7 @@ class Input extends Component {
             value={this.state.text}
             type="text"
             placeholder="Enter your message and press ENTER"
-            autoFocus="true"
+            autoFocus
           />
           <button>Send</button>
         </form>

3ea05f21da8c309bd65d3ab506327c1c8127c83b
Marin Benčević <marinbenc@gmail.com>
2023-09-01 13:55:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['typing-indicator']
+  transpilePackages: ['typing-indicator'],
+  assetPrefix: './',
 }
 
 module.exports = nextConfig

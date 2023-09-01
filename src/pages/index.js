e12340e8d9706224577834dbce4d6642dce5543a
marinbenc <marinbenc@gmail.com>
2023-09-01 08:46:13 +0200

    Starter code

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,138 +4,17 @@ import Script from 'next/script';
 
 import { useState, useEffect, useRef } from 'react';
 
-import Members from '@/components/Members'
-import Messages from '@/components/Messages'
-import Input from '@/components/Input'
-import TypingIndicator from '@/components/TypingIndicator'
-
-function randomName() {
-  const adjectives = [
-    'autumn', 'hidden', 'bitter', 'misty', 'silent', 'empty', 'dry', 'dark',
-    'summer', 'icy', 'delicate', 'quiet', 'white', 'cool', 'spring', 'winter',
-    'patient', 'twilight', 'dawn', 'crimson', 'wispy', 'weathered', 'blue',
-    'billowing', 'broken', 'cold', 'damp', 'falling', 'frosty', 'green', 'long',
-    'late', 'lingering', 'bold', 'little', 'morning', 'muddy', 'old', 'red',
-    'rough', 'still', 'small', 'sparkling', 'shy', 'wandering',
-    'withered', 'wild', 'black', 'young', 'holy', 'solitary', 'fragrant',
-    'aged', 'snowy', 'proud', 'floral', 'restless', 'divine', 'polished',
-    'ancient', 'purple', 'lively', 'nameless'
-  ];
-  const nouns = [
-    'waterfall', 'river', 'breeze', 'moon', 'rain', 'wind', 'sea', 'morning',
-    'snow', 'lake', 'sunset', 'pine', 'shadow', 'leaf', 'dawn', 'glitter',
-    'forest', 'hill', 'cloud', 'meadow', 'sun', 'glade', 'bird', 'brook',
-    'butterfly', 'bush', 'dew', 'dust', 'field', 'fire', 'flower', 'firefly',
-    'feather', 'grass', 'haze', 'mountain', 'night', 'pond', 'darkness',
-    'snowflake', 'silence', 'sound', 'sky', 'shape', 'surf', 'thunder',
-    'violet', 'water', 'wildflower', 'wave', 'water', 'resonance', 'sun',
-    'wood', 'dream', 'cherry', 'tree', 'fog', 'frost', 'voice', 'paper', 'frog',
-    'smoke', 'star'
-  ];
-  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  return adjective + noun;
-}
-
-function randomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
-}
-
-let drone = null
-
 export default function Home() {
-  const [messages, setMessages] = useState([]);
-  const [members, setMembers] = useState([]);
-  const [me, setMe] = useState({
-    username: randomName(),
-    color: randomColor(),
-  });
-
-  const messagesRef = useRef();
-  messagesRef.current = messages;
-  const membersRef = useRef();
-  membersRef.current = members;
-  const meRef = useRef();
-  meRef.current = me;
-
-  function connectToScaledrone() {
-    drone = new window.Scaledrone('YOUR-CHANNEL-ID', {
-      data: meRef.current,
-    });
-    drone.on('open', error => {
-      if (error) {
-        return console.error(error);
-      }
-      meRef.current.id = drone.clientId;
-      setMe(meRef.current);
-    });
-
-    const room = drone.subscribe('observable-room');
-
-    room.on('message', message => {
-      const {data, member} = message;
-      if (typeof data === 'object' && typeof data.typing === 'boolean') {
-        const newMembers = [...membersRef.current];
-        const index = newMembers.findIndex(m => m.id === member.id);
-        newMembers[index].typing = data.typing;
-        setMembers(newMembers);
-      } else {
-        setMessages([...messagesRef.current, message]);
-      }
-    });
-    room.on('members', members => {
-      setMembers(members);
-    });
-    room.on('member_join', member => {
-      setMembers([...membersRef.current, member]);
-    });
-    room.on('member_leave', ({id}) => {
-      const index = membersRef.current.findIndex(m => m.id === id);
-      const newMembers = [...membersRef.current];
-      newMembers.splice(index, 1);
-      setMembers(newMembers);
-    });
-  }
-
-  useEffect(() => {
-    if (drone === null) {
-      connectToScaledrone();
-    }
-  }, []);  
-
-  function onSendMessage(message) {
-    drone.publish({
-      room: 'observable-room',
-      message
-    });
-  }
-
-  function onChangeTypingState(isTyping) {
-    drone.publish({
-      room: 'observable-room',
-      message: {typing: isTyping}
-    });
-  }
-
   return (
     <>
       <Head>
         <title>Scaledrone Chat App</title>
         <meta name='description' content='Your brand-new chat app!' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <script type='text/javascript' src='https://cdn.scaledrone.com/scaledrone.min.js' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles.app}>
         <div className={styles.appContent}>
-          <Members members={members} me={me}/>
-          <Messages messages={messages} me={me}/>
-          <TypingIndicator members={members.filter(m => m.typing && m.id !== me.id)}/>
-          <Input
-            onSendMessage={onSendMessage}
-            onChangeTypingState={onChangeTypingState}
-          />
-          <a className={styles.upsell} href='https://www.scaledrone.com/blog/tutorial-build-a-reactjs-chat-app/'>Real-time React chat using Scaledrone. See full tutorial →</a>
         </div>
       </main>
     </>
